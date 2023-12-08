615ce2670de3060274d75a2059ae419516cdb4db
Yofr3H <yofregarcia@gmail.com>
2023-12-08 08:55:17 -0600

    ok3

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using VaccineStockManagementWithMDPs
 using Test
-using PlotlyJS, DataFrames, CSV
+using PlotlyJS
+using DataFrames
+using CSV
 
 @testset "VaccineStockManagementWithMDPs.jl" begin
     @test(
@@ -9,7 +11,7 @@ using PlotlyJS, DataFrames, CSV
     )
     p = VaccineStockManagementWithMDPs.load_parameters()
     p_sto = VaccineStockManagementWithMDPs.get_stochastic_perturbation()
-    t = 90
+    t = 90.0
     a_t = 0.25
     k = .0015
     S = p.S_0[1]
@@ -46,16 +48,55 @@ using PlotlyJS, DataFrames, CSV
         VaccineStockManagementWithMDPs.rhs_evaluation!(
             t, x_df, opt_policy,a_t, k, p
          )
+    x_c = 
+        VaccineStockManagementWithMDPs.get_vaccine_stock_coverage(
+            k,p
+        )
+
     x_new_df =  DataFrame(
         Dict(
             zip(header_strs, x_new)
         )
     )
+    k_0 = p.k_stock[1] / p.N[1]
+    time_horizon_1 = p.t_delivery[2]
+    N_grid_size = p.N_grid_size[1]
+    t_interval_1 = LinRange(0, time_horizon_1, N_grid_size)
     #x01, df1 = VaccineStockManagementWithMDPs.get_solution_path!(p)
     #@test((x01.D[1] + x01.E[1] + x01.I_A[1] + x01.I_S[1] + x01.R[1] + x01.S[1] + x01.V[1]) == x01.CL[1]
     #)
+    sol_1 = VaccineStockManagementWithMDPs.get_interval_solution!(
+                   t_interval_1,
+                   x_df,
+                   opt_policy, 
+                   a_t,
+                   k_0,
+                   p
+               )
+    #opt_sol_1 = sol_1
+    #prefix = "df_sol_"
+    #sufix = "1.csv"
+    #file = "./data/" * prefix * sufix
+    #df_sol_1 = VaccineStockManagementWithMDPs.save_interval_solution(
+    #    opt_sol_1;
+    #    file_name = file
+    #)
+    #rand_p = VaccineStockManagementWithMDPs.get_stochastic_perturbation()
+    #@test(
+    #    rand_p.t_delivery[2] <= rand_p.t_delivery[3] 
+    #) 
+    #@test(
+    #    df_sol_1.time[end] == 80.0
+    #)
     @test(
-        VaccineStockManagementWithMDPs.get_vaccine_stock_coverage(k,p) ==0.0014243754403948964 
+        sol_1[end,13] == 0.023156632531811085          
+    )
+    VaccineStockManagementWithMDPs.get_solution_path!(p)
+    @test(
+        VaccineStockManagementWithMDPs.get_vaccine_stock_coverage(k,p) == 0.0014243754403948964 
+    )
+    @test(
+        VaccineStockManagementWithMDPs.get_vaccine_action!(x_c,t,p) == 1.5837675860020853e-5
     )
     @test(
         VaccineStockManagementWithMDPs.load_parameters().N_grid_size[1] == 500
